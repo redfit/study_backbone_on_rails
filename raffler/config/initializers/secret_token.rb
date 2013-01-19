18b118686c1f8b425ff171ecffa23b77ea29b0d1
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2013-01-19 21:21:47 +0900

    rails new raffler

---
+++ raffler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'e0ae3ce09a6af9d6c0f7bed69958952d9837f5012ebafad76b51b5b2f4e6c329100e99e3d62289e3cc143c0e1c6204d66f9a242f8631bfd3e34793371f398105'
